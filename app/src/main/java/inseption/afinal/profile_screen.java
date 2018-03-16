84753c427a2711e070d967c971083c4d2c868044
up819025 <35937648+up819025@users.noreply.github.com>
2018-03-16 13:46:51 +0000

    Added Profile screen

---
+++ app/src/main/java/inseption/afinal/profile_screen.java
@@ -0,0 +1,31 @@
+package inseption.afinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class profile_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Button CancelButton = (Button) findViewById(R.id.cancel_button);
+        CancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(profile_screen.this, main_screen.class));
+            }
+        });
+    }
+
+}
