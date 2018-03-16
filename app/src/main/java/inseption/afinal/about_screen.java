8e95e7b691d951f85813ed8fbe04a1852344c4a5
BakkerCSc <up776681@myport.ac.uk>
2018-03-16 19:16:29 +0000

    Added about screen

---
+++ app/src/main/java/inseption/afinal/about_screen.java
@@ -0,0 +1,30 @@
+package inseption.afinal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class about_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button BackButton = (Button) findViewById(R.id.back_button);
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(about_screen.this, main_screen.class));
+            }
+        });
+    }
+
+}
